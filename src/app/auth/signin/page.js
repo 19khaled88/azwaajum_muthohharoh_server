e9ae0f161646ed09f0a88e9987418921db64740e
19khaled88 <kkhaled88hasan@gmail.com>
2023-09-21 14:52:51 +0600

    create sign in & sign up page

--- src/app/auth/signin/page.js
+++ src/app/auth/signin/page.js
@@ -1,15 +1,15 @@
 'use client'
 import React, { useState } from 'react'
-import signUp from '../../firebase/auth/signup'
+import signin from '../../firebase/auth/signin'
 import { useRouter } from 'next/navigation'
 
-const Signup = () => {
+const Signin = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const router = useRouter()
   const handleForm = async (event) => {
     event.preventDefault()
-    const { result, error } = await signUp(email, password)
+    const { result, error } = await signin(email, password)
 
     if (error) return error
   }
@@ -17,7 +17,7 @@ const Signup = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-between">
       <div className="m-auto form-wrapper border border-black-600">
-        <h1 className="text-center text-2xl font-medium">Sign up</h1>
+        <h1 className="text-center text-2xl font-medium">Sign In</h1>
         <form onSubmit={handleForm} className="form p-2 flex flex-col drop-shadow-md">
           <label htmlFor="email">
             <p className='pb-1'>Email</p>
@@ -43,11 +43,11 @@ const Signup = () => {
               placeholder="password"
             />
           </label>
-          <button type="submit" className='bg-sky-400 mt-6 px-1 py-1 text-white hover:bg-blue-400'>Sign up</button>
+          <button type="submit" className='bg-sky-400 mt-6 px-1 py-1 text-white hover:bg-blue-400'>Sign In</button>
         </form>
       </div>
     </div>
   )
 }
 
-export default Signup
+export default Signin
